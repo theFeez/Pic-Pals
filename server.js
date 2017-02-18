e4bac5e815c8b58d32936e8b9d2d4d07b40303ad
theFeez <zachfeez@knights.ucf.edu>
2017-02-18 04:36:15 -0500

    added /login route and got upload working

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 var multer = require('multer');
+var config = require('./config');
+var configInstance = config;
+var cloudinary = require('cloudinary');
+var path= require('path');
+var cors = require('cors');
+var request = require('request');
+var bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true,limit:'50mb'}));
+app.use(bodyParser.json({limit:'50mb'}));
+
+cloudinary.config({ 
+  cloud_name: configInstance.cloudName, 
+  api_key: configInstance.cloudKey, 
+  api_secret: configInstance.cloudSecret 
+});
 
 var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    cb(null, __dirname+'/pics')
+  },
   filename: function (req, file, cb) {
       if(path.extname(file.originalname.toLowerCase())==='.jpg'||path.extname(file.originalname.toLowerCase())==='.jpeg'||path.extname(file.originalname.toLowerCase())==='.gif'||path.extname(file.originalname.toLowerCase())==='.bmp'||path.extname(file.originalname.toLowerCase())==='.png')
           {
@@ -15,8 +34,19 @@ var storage = multer.diskStorage({
 var upload = multer({storage})
 
 
+
 app.get('/',function(req,res){
-   res.send('req recieved'); 
+    res.sendFile(__dirname+'/index.html');
+});
+
+app.get('/load',function(req,res){
+    request('http://res.cloudinary.com/haklhguz5/image/upload/v1487404456/aj9zetaz3wi4a8mnit8r.jpg', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body) // Print the google web page.
+    res.send(body);
+  }
+})
+   
 });
 
 app.post('/upload',upload.single('image'),function(req,res){
@@ -27,9 +57,21 @@ app.post('/upload',upload.single('image'),function(req,res){
     }
     else{
         //deal with the picture
+        console.log('acceptable image recieved');
+        /*cloudinary.uploader.upload(req.file.path,function(result){
+            console.log(result);
+            res.redirect('/');
+        })*/
+        
     }
     
 })
+
+app.post('/login',function(req,res){
+    console.log(req.body.username);
+    console.log(req.body.password);
+    
+});
 
 
 app.listen(process.env.PORT||500,function(){
