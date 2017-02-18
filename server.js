97b6b74b1f02327d79b596e601147e56b517aef5
theFeez <zachfeez@knights.ucf.edu>
2017-02-18 01:57:11 -0500

    added gitignore

--- server.js
+++ server.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var app = express();
+var multer = require('multer');
+
+var storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+      if(path.extname(file.originalname.toLowerCase())==='.jpg'||path.extname(file.originalname.toLowerCase())==='.jpeg'||path.extname(file.originalname.toLowerCase())==='.gif'||path.extname(file.originalname.toLowerCase())==='.bmp'||path.extname(file.originalname.toLowerCase())==='.png')
+          {
+              cb(null, file.originalname)
+          }
+    
+  }
+});
+
+var upload = multer({storage})
+
 
 app.get('/',function(req,res){
    res.send('req recieved'); 
 });
+
+app.post('/upload',upload.single('image'),function(req,res){
+    console.log('upload request recieved');
+    if(req.file===undefined){
+        console.log(req.body);
+        res.end();
+    }
+    else{
+        //deal with the picture
+    }
+    
+})
 
 
 app.listen(process.env.PORT||500,function(){
